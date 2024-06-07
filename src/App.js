cc369242424868bf979c66c80e590aeabb46ffd6
musabnaik <musab944@gmail.com>
2024-06-07 21:13:56 +0400

    Changes to hide About App and How We Use Your Data after sign in

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import FileUpload from './FileUpload';
 
 function App() {
   const [accessToken, setAccessToken] = useState('');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const clientId = '761043493236-mpvo1l2ppqusk3jt7t6o3jjv8tnnt8vs.apps.googleusercontent.com';
 
   const onSuccess = (response) => {
     console.log('Google authentication successful:', response);
     setAccessToken(response.access_token);
+    setIsAuthenticated(true);
   };
 
   const onError = (error) => {
@@ -23,14 +25,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 style={{ marginBottom: '5px' }}>Encrypted Google Drive</h1>        
-        <table style={{ marginTop: '5px' }}>    
+        <table style={{ marginTop: '5px' }}> 
+          <td><a href="https://musabnaik.com/Encrypted-Drive/" style={{ color: 'white', fontSize: '18px' }}>Home</a></td>
+          <td>  </td>
+          <td>  </td>
+          <td>  </td>   
           <td><a href="https://musabnaik.com/privacy-policy" target="_blank" rel="noopener noreferrer" style={{ color: 'white', fontSize: '18px' }}>Privacy Policy</a></td>
           <td>  </td>
           <td>  </td>
           <td>  </td>
           <td><a href="https://musabnaik.com" target="_blank" rel="noopener noreferrer" style={{ color: 'white', fontSize: '18px' }}>Musab Naik</a></td>
         </table>
-
+        
+        {!isAuthenticated && (
+          <>
             <h4 style={{marginBottom: '0px',}}>About App</h4>
             <p style={{fontSize: '20px', width: '70%', textAlign: 'center', marginTop: '0px', marginBottom: '0px'}}>
               This app allows you to securely encrypt your files locally before uploading them to your Google Drive. 
@@ -44,6 +52,9 @@ function App() {
               or folders in your Google Drive. All encryption and decryption processes occur locally on your device, 
               ensuring that unencrypted data and passwords never leave your device.
             </p>
+          </>
+          )
+        }
         
         <br></br>
         <GoogleOAuthProvider clientId={clientId}>
